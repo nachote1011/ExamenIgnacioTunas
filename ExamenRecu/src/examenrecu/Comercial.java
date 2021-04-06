2505906d9d1308c244928b5e4f8a5fded585ec9c
admin <admin@Win7_1TB>
2021-04-06 11:43:17 +0200

    Muestro la array por pantalla

---
+++ ExamenRecu/src/examenrecu/Comercial.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examenrecu;
+
+/**
+ *
+ * @author admin
+ */
+public class Comercial {
+    private String nombre;
+    private double sueldo;
+
+    public Comercial(String nombre, double sueldo) {
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+    }
+    
+}
